40d1b0444cdf23013740b66780b02108713bafb7
Zahid <zahid@zmdev.com>
2023-02-20 10:47:23 +0000

    tweaks to homepage UI for iOS

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,19 @@ import Start from "@/components/home/start";
 export default function Home() {
 	return (
 		<section className="w-screen h-screen relative">
-			<div className="flex min-h-screen flex-col items-center justify-center p-10">
+			<div className="flex min-h-screen flex-col items-center justify-center py-10 px-4">
 				<img src="/assets/beams.jpg" alt="" className="absolute w-full h-full object-cover" />
 				<div className="absolute inset-0 bg-[url(/assets/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
 				<div className="z-10 w-full max-w-screen-sm">
 					<div className="">
 						<div className="flex flex-row space-x-1 items-center justify-center">
-							<h1 className="text-center text-xl font-bold tracking-tight sm:text-5xl">Transvribe</h1>
-							<svg xmlns="http://www.w3.org/2000/svg" className="w-14 h-14 mt-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+							<h1 className="text-center text-3xl font-bold tracking-tight sm:text-5xl">Transvribe</h1>
+							<svg xmlns="http://www.w3.org/2000/svg" className="w-14 h-14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
 								<path d="m22 8-6 4 6 4V8Z" />
 								<rect x={2} y={6} width={14} height={12} rx={2} ry={2} />
 							</svg>
 						</div>
-						<p className="mt-3 text-center text-lg text-black/50">Search any video, powered by AI embeddings</p>
+						<p className="mt-1 md:mt-3 text-center text-sm md:text-lg text-black/50">Search any video, powered by AI embeddings</p>
 					</div>
 					<Start />
 				</div>
