c2fafaa86a2921743fad002769c0c6e0999da1f6
Zahid <zahid@zmdev.com>
2023-02-20 10:43:56 +0000

    implemented video header

--- components/youtube-video/video-header/index.js
+++ components/youtube-video/video-header/index.js
@@ -1,23 +1,27 @@
 export default function VideoHeader({ video }) {
     return (
         <header
-            className="fixed top-0 left-0 z-10 relative py-2 bg-white w-full shadow text-gray-700"
+            className="fixed top-0 left-0 z-20 py-2 bg-white w-full shadow text-gray-700"
         >
-            <div className="w-full max-w-5xl px-2 flex flex-row space-x-4">
+            <a
+                className="w-full max-w-5xl px-2 flex flex-row space-x-4 mx-auto"
+                href={video.url}
+                target="_blank"
+            >
                 <figure
-                    className="w-3/12 aspect-video relative shrink-0"
+                    className="w-3/12 md:w-2/12 aspect-video relative shrink-0"
                 >
                         <img src={video.thumb_url} className="w-full h-full object-cover" />
                 </figure>
                 <div className="w-full">
-                    <p className="font-semibold text-sm">
-                        {video.title}
+                    <p className="font-semibold text-sm md:text-2xl">
+                        {video.title.slice(0, 55)}{video.title.length > 55 ? '...' : ''}
                     </p>
                     <p className="font-base text-xs text-gray-500">
                         by {video.author}
                     </p>
                 </div>
-            </div>
+            </a>
         </header>
     )
 }
