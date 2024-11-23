5eecd2a1c1b2dbe8842fc219756c0af6ffd3681e
Zahid <zahid@zmdev.com>
2024-11-23 18:32:59 +0000

    updated message for YouTube caption issue

--- components/home/start/english-captions-modal.js
+++ components/home/start/english-captions-modal.js
@@ -1,15 +1,16 @@
 export default function EnglishCaptionsModal({ onClose }) {
     return (
-        <div className="w-full h-full py-14 px-10 flex flex-col text-gray-700 flex flex-col justify-center">
+        <div className="w-full h-full py-14 px-10 text-gray-700 flex flex-col justify-center">
             <div className="w-full flex flex-row justify-center mb-3">
                 <img src="/assets/monkey-english.png" className="w-32 md:w-40" />
             </div>
             <p className="md:text-3xl font-bold text-center text-gray-900">
-                This video is not supported... yet!
+                YouTube captions unavailable
             </p>
             <p className="mt-4">
-                For now Transvribe only works with YouTube videos that have English captions. We're working
-                on adding support for more languages.
+                Transvribe works by scraping captions from YouTube. YouTube has recently changed thir API so this
+                is no longer possible. I'm working on implementing OpenAI Whisper or similar to continue serving
+                Transvribe.
             </p>
             <p className="mt-3">
                 Stay tuned for updates on Twitter: <a className="underline" href="https://twitter.com/zaarheed" target="_blank">@transvribe</a>.
